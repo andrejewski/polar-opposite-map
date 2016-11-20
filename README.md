46fc2e38b5d143fe70851384258c2bb6f8c361a6
Chris Andrejewski <christopher.andrejewski@gmail.com>
2016-11-20 11:46:25 -0500

    real initial commit
    if no typos I am done

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# polar-opposite-map
+# Polar Opposite Map
 What is on the exact other side of the planet
+
+![Looking at Antipodes Island](https://raw.github.com/andrejewski/polar-opposite-map/master/screenshot.png)
+
+The correct term is [Antipodes](https://en.wikipedia.org/wiki/Antipodes) but everyone is already dominating the Google search pages for that. I have been wanting to make this for awhile. It seems to be the only one that is real-time.
+
+Unfortunately where I live didn't give me any good results. Most places are polar opposites with water, which is not too exciting.
---
+++ docs/index.js
@@ -0,0 +1,50 @@
+'use strict'
+
+var defaultLocation = L.latLng(51.505, -0.09)
+var localMap = L.map('local-map')
+var remoteMap = L.map('remote-map')
+
+var localLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+})
+var remoteLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+})
+
+localLayer.addTo(localMap)
+remoteLayer.addTo(remoteMap)
+
+function opposite (latlng) {
+  var lng = latlng.lng > 0 ? 180 - latlng.lng : 180 + latlng.lng
+  return L.latLng(-latlng.lat, lng)
+}
+
+localMap.setView(defaultLocation, 13)
+remoteMap.setView(opposite(defaultLocation), 13)
+
+var isCursorLocal
+
+$('#local-map').on('mouseenter', function () {
+  isCursorLocal = true
+})
+$('#remote-map').on('mouseenter', function () {
+  isCursorLocal = false
+})
+
+localMap.on('move', function () {
+  if (!isCursorLocal) return
+  var latlng = localMap.getCenter()
+  var zoom = localMap.getZoom()
+  remoteMap.setView(opposite(latlng), zoom)
+})
+
+remoteMap.on('move', function () {
+  if (isCursorLocal) return
+  var latlng = remoteMap.getCenter()
+  var zoom = remoteMap.getZoom()
+  localMap.setView(opposite(latlng), zoom)
+})
+
+$('#auto-locate').on('click', function () {
+  localMap.locate({setView: true})
+})
